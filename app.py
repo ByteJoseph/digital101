97cf6422d6e7640ecae941e5a83f8c69f51c424f
ByteJoseph <josephnjoseph43@gmail.com>
2025-04-05 06:03:54 +0530

    init: added pages
    init: added pages

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.set_page_config(page_title="Digital 101 - Revision",page_icon=":material/home:")
+pg = st.navigation([st.Page("./pages/1_home.py",icon="👋"),st.Page("./pages/2_learn.py")])
+pg.run()
---
+++ pages/1_home.py
@@ -0,0 +1,17 @@
+import streamlit as st
+st.title("Welcome to")
+st.markdown("""
+            ## **Digital 101** **:rainbow[Revision]**
+            """)
+st.write(
+    """
+    This is the perfect place to revisit and strengthen your understanding 
+    of everything you’ve learned so far in Digital 101.
+
+    This app also includes an **Internet Time Machine** that lets you explore 
+    blogs which were deleted by the author and are no longer accessible through the course.
+    """
+)
+if st.button("Learn",icon=":material/menu_book:",):
+    st.switch_page("./pages/2_learn.py")
+    pass
