db0da5279e4cd6c420a31fade393055335eca47b
ByteJoseph <josephnjoseph43@gmail.com>
2025-04-05 16:04:39 +0530

    feat: added link to dataset
    added link to dataset

--- pages/1_home.py
+++ pages/1_home.py
@@ -13,6 +13,7 @@ st.write(
     -->
     """,unsafe_allow_html=True
 )
+st.markdown("[**View my dataset**](https://github.com/ByteJoseph/digital101/tree/master/dataset)")
 if st.button("Revise Topics",icon=":material/menu_book:",):
     st.switch_page("./pages/2_learn.py")
     pass
