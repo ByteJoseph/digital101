2baacb7d3beeb7e236055119f0ae08cf2dc92ca8
ByteJoseph <josephnjoseph43@gmail.com>
2025-04-06 09:28:30 +0530

    fix: improve ui
    improved ui

--- pages/1_home.py
+++ pages/1_home.py
@@ -7,16 +7,17 @@ st.write(
     """
     This is the perfect place to revisit and strengthen your understanding 
     of everything you’ve learned so far in Digital 101.
+    You can view my dataset [**here**](https://github.com/ByteJoseph/digital101/tree/master/dataset).
 
   <!--  This app also includes an **Internet Time Machine** that lets you explore 
     blogs which were deleted by the author and are no longer accessible through the course.
     -->
     """,unsafe_allow_html=True
 )
-st.markdown("[**View my dataset**](https://github.com/ByteJoseph/digital101/tree/master/dataset)")
+
 st.balloons()
-if st.button("Revise Topics",icon=":material/menu_book:",):
+if st.button("Revise Topics",icon=":material/menu_book:",type="primary"):
     st.switch_page("./pages/2_learn.py")
     pass
 st.markdown("---")
-st.markdown("Made with 💛 by [**Joseph**](https://github.com/ByteJoseph)")
+st.markdown("<p style='text-align: center;'>Made with 💛 by <a href='https://github.com/ByteJoseph'><b>Joseph</b></a></p>", unsafe_allow_html=True)
