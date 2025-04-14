c4c8d1bdb1d2f0287af40b10fdbfafe44f906bc3
ByteJoseph <josephnjoseph43@gmail.com>
2025-04-14 11:22:50 +0530

    fix: improve system
    improved system

--- pages/2_learn.py
+++ pages/2_learn.py
@@ -154,7 +154,7 @@ for i, row in enumerate(df.itertuples(), start=1):
           st.markdown(f"**Generated Summary:** {summary}", unsafe_allow_html=True)
     else:
         with st.spinner("Wait, Generating.."):
-          prompt = f"Summarize the content from this URL in detail without losing key information. Do not mention the blog source or include any tables.\n\nURL: {row.URL} include points to revise faster,TL;DR, and Did you know? etc"
+          prompt = f"Summarize the content from this URL in detail without losing key information. Do not mention the blog source or include any tables.\n\nURL: {row.URL} include points to revise faster,Act like TL;DR, and Did you know? etc"
           summary = get_gemini_summary(prompt)
           st.markdown(f"**Generative Summary:** {summary}", unsafe_allow_html=True)
     st.markdown("---")
